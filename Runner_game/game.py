be1d97d042c63a5daf8fcdae1629aadf7172d9ea
sarang80040 <sarangmeshte29@gmail.com>
2026-02-20 11:03:39 +0530

    added starting screen and ending screen with score

--- Runner_game/game.py
+++ Runner_game/game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import torch
+import numpy
 pygame.init()
 pygame.display.set_caption("SaranGame")
 screen = pygame.display.set_mode((800, 400))
@@ -8,10 +9,11 @@ clock =pygame.time.Clock() #for controlling framerate - fps
 test_font=pygame.font.Font('Pixeltype.ttf',50)
 test_surface=pygame.Surface((100,200))
 test_surface.fill((255,100,200))
-game_active=True
+game_active=False
 start_time=0
 current_time=0
 def display_score():
+    global current_time,score_surface,score_rect
     current_time=pygame.time.get_ticks()-start_time
     score_surface=test_font.render(f'Score: {current_time}',False,(64,64,64))
     score_rect=score_surface.get_rect(center=(400,50))
@@ -30,6 +32,18 @@ player_surface=pygame.image.load("player_walk_1.png").convert_alpha()
 player_rect=player_surface.get_rect(midbottom=(80,250))
 player_gravity=0
 
+player_stand=pygame.image.load("player_stand.png").convert_alpha()
+player_stand=pygame.transform.rotozoom(player_stand,0,2) #image,degrees,scale
+player_stand_rect=player_stand.get_rect(center=(400,200))
+
+game_name=test_font.render("SarGame",False,(100,60,50))
+game_name_rect=game_name.get_rect(center=(400,80))
+
+game_message=test_font.render("Press space to run",False,(100,60,50))
+game_message_rect=game_message.get_rect(center=(400,350))
+
+maxscore=0
+count=False
 while True:
     #draw all our elements
     #update everything on the screen
@@ -77,13 +91,19 @@ while True:
             # pygame.quit()
             # sys.exit()
             game_active=False
+            count=True
     
     else:
-        
-        score_surface=test_font.render("Game Over",False,(64,64,64))
-        score_rect=score_surface.get_rect(center=(400,50))
-        screen.blit(score_surface,score_rect)
-        
+        screen.fill((94,129,162))
+        screen.blit(player_stand,player_stand_rect)
+        maxscore=max(maxscore,current_time)
+        screen.blit(game_message,game_message_rect)
+        if(count):
+            score_message=test_font.render(f'Your score: {current_time} \tMax score: {maxscore}',False,(100,60,50))
+            score_message_rect=score_message.get_rect(center=(400,30))
+            screen.blit(score_message,score_message_rect)
+        else : 
+            screen.blit(game_name,game_name_rect)
    
         
     
